9767b2f7e3bb1b5be1676bc5b411d832188708df
Jason McClellan <jason@jasonmcclellan.net>
2014-01-20 11:31:53 -0500

    Update namespace

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Discovery\Bundle\AwsBundle\DependencyInjection;
+namespace JLM\AwsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
