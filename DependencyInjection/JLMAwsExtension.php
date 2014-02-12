7a2b4985cc1805c9500eac837f9348687e13f8fb
Jason McClellan <jason@jasonmcclellan.net>
2014-02-11 23:09:37 -0500

    Closes #20 // Removed YAML Loader hack in favor of duplicating service reference logic as it was a small method

--- DependencyInjection/JLMAwsExtension.php
+++ DependencyInjection/JLMAwsExtension.php
@@ -4,14 +4,16 @@ namespace JLM\AwsBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\ExpressionLanguage\Expression;
 
 use Symfony\Component\Config\FileLocator;
 
 use JLM\AwsBundle\Config\AwsConfigTranslator;
 use JLM\AwsBundle\Aws\Common\Aws;
-use Symfony\Component\Yaml\Dumper;
+
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -33,63 +35,33 @@ class JLMAwsExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {       
-        //die(json_encode($configs, JSON_PRETTY_PRINT));
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
-        //die(json_encode($config, JSON_PRETTY_PRINT));
         
         $awsConfigTranslator = new AwsConfigTranslator();
         $awsConfig = $awsConfigTranslator->translateConfigToAwsConfig($config);
 
-        $awsServices = $this->generateServicesConfigArray($awsConfig, $awsConfigTranslator);
-
-        //die(json_encode($awsServices,JSON_PRETTY_PRINT));
-        $dumper = new Dumper();
-
-        /*
-         * We are currently doing this so we do not have to re-implement
-         * the service lookup logic found in the YAMLFileLoader, however
-         * this is dirty and it's far cleaner to just re-implement that
-         * small bit of logic that is unlikely to change.
-         */
-        $yaml = $dumper->dump($awsServices);
-
-        $tmpFile = tempnam(sys_get_temp_dir(), 'JLMAwsBundle');
-
-        $fp = fopen($tmpFile, 'w', false);
-        fwrite($fp, $yaml);
-        fclose($fp);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator());
-        $loader->load($tmpFile);
+        $this->generateServices($awsConfig, $container);
     }
 
-    private function generateServicesConfigArray(array $awsConfig, AwsConfigTranslator $awsConfigTranslator)
+    private function generateServices(array $awsConfig, ContainerBuilder $container)
     {
-        $awsServices['services'] = array(
-                self::AWS_SERVICE_PREFIX . 'aws' => array(
-                    'class' => self::BASE_CLASS,
-                    'factory_class' => self::BASE_CLASS,
-                    'factory_method' => 'factory',
-                    'arguments' => array($awsConfig)
-                )
-            );
+        $awsConfig = $this->resolveServices($awsConfig);
+        $aws = new Definition(self::BASE_CLASS, array($awsConfig));
+        $aws->setFactoryClass(self::BASE_CLASS);
+        $aws->setFactoryMethod('factory');
+        $container->setDefinition('jlm_aws.aws', $aws);
 
         foreach ($awsConfig['services'] as $service => $serviceConfig) {
             if (!isset($serviceConfig['class'])) {
                 continue; // Skip any configurations lacking a class as they are abstract
             }
-            
-            $awsServices['services'][self::AWS_SERVICE_PREFIX . $service] = array(
-                    'class' => $serviceConfig['class'],
-                    'factory_service' => self::AWS_SERVICE_PREFIX . 'aws',
-                    'factory_method' => 'get',
-                    'arguments' => array($service)
-                );         
-        }
 
-        return $awsServices;
+            $definition = new Definition($serviceConfig['class'], array($service));
+            $definition->setFactoryService(self::AWS_SERVICE_PREFIX . 'aws');
+            $definition->setFactoryMethod('get');
+            $container->setDefinition(self::AWS_SERVICE_PREFIX . $service, $definition);       
+        }
     }
 
     public function getXsdValidationBasePath()
@@ -101,4 +73,51 @@ class JLMAwsExtension extends Extension
     {
         return 'http://symfony.com/schema/dic/jlm-aws';
     }
+
+     /**
+     * Resolves services.
+     *
+     * This is taken directly from Symfony\Component\DependencyInjection\Loader\YamlFileLoader
+     *
+     * There was no way to re-use this without some ugly hack involving writing our config out to
+     * YML first on-the-fly which, while it worked, was not a better solution than simply repeating
+     * this small bit of logic here.
+     *
+     * @param string $value
+     *
+     * @return Reference
+     */
+    private function resolveServices($value)
+    {
+        if (is_array($value)) {
+            $value = array_map(array($this, 'resolveServices'), $value);
+        } elseif (is_string($value) &&  0 === strpos($value, '@=')) {
+            return new Expression(substr($value, 2));
+        } elseif (is_string($value) &&  0 === strpos($value, '@')) {
+            if (0 === strpos($value, '@@')) {
+                $value = substr($value, 1);
+                $invalidBehavior = null;
+            } elseif (0 === strpos($value, '@?')) {
+                $value = substr($value, 2);
+                $invalidBehavior = ContainerInterface::IGNORE_ON_INVALID_REFERENCE;
+            } else {
+                $value = substr($value, 1);
+                $invalidBehavior = ContainerInterface::EXCEPTION_ON_INVALID_REFERENCE;
+            }
+
+            if ('=' === substr($value, -1)) {
+                $value = substr($value, 0, -1);
+                $strict = false;
+            } else {
+                $strict = true;
+            }
+
+            if (null !== $invalidBehavior) {
+                $value = new Reference($value, $invalidBehavior, $strict);
+            }
+        }
+
+        return $value;
+    }
+
 }
